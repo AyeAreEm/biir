a8816b02247facc5a33a9a71feca77517a576723
AyeAreEm <ag.ayeareem@gmail.com>
2024-12-25 20:24:51 +1000

    add substraction, no negative numbers tho

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 struct BigInt {
     buf: Vec<u8>,
@@ -47,13 +47,7 @@ impl Add<BigInt> for BigInt {
         }
 
         for (i, self_digit) in self.buf.iter().rev().enumerate() {
-            let rhs_digit = match rhs.buf.iter().rev().nth(i) {
-                Some(d) => d,
-                None => {
-                    temp.push(self_digit.clone() + carry);
-                    continue;
-                },
-            };
+            let rhs_digit = rhs.buf.iter().rev().nth(i).unwrap();
             let new_digit = self_digit + rhs_digit + carry;
             carry = (new_digit / 10) as u8 % 10;
             let unit = new_digit % 10;
@@ -67,7 +61,74 @@ impl Add<BigInt> for BigInt {
         }
 
         temp.reverse();
-        BigInt{ buf: temp.clone() }
+        BigInt{ buf: temp }
+    }
+}
+
+impl Sub<BigInt> for BigInt {
+    type Output = BigInt;
+
+    fn sub(self, mut rhs: BigInt) -> BigInt {
+        let mut temp: Vec<u8> = Vec::new();
+
+        if self.buf.len() != rhs.buf.len() {
+            if self.buf.len() > rhs.buf.len() {
+                let diff = self.buf.len() - rhs.buf.len();
+                for _ in 0..diff {
+                    rhs.buf.insert(0, 0);
+                }
+            }
+        }
+
+        let mut decrease = false;
+        for (i, self_digit) in self.buf.iter().rev().enumerate() {
+            let rhs_digit = rhs.buf.iter().rev().nth(i).unwrap();
+
+            let digit = if decrease {
+                if self_digit == &0 {
+                    9
+                } else {
+                    self_digit - 1
+                }
+            } else {
+                *self_digit
+            };
+
+            let new_digit = if digit > *rhs_digit {
+                digit - rhs_digit
+            } else if digit == *rhs_digit {
+                0
+            } else {
+                println!("digit: {digit}, rhs_digit: {rhs_digit}");
+                decrease = true;
+                (digit + 10) - rhs_digit
+            };
+            temp.push(new_digit);
+        }
+
+        temp.reverse();
+        let mut res = Vec::new();
+        let mut seen_non_zero = false;
+        let mut zeros = Vec::new();
+        for num in &temp {
+            if num == &0 && !seen_non_zero {
+                zeros.push(num.clone());
+                continue;
+            }
+
+            if num == &0 && seen_non_zero {
+                res.push(num.clone());
+            } else {
+                seen_non_zero = true;
+                res.push(num.clone());
+            }
+        }
+
+        if !seen_non_zero && zeros.len() == 1 {
+            res = vec![0];
+        }
+
+        BigInt { buf: res }
     }
 }
 
@@ -75,8 +136,8 @@ fn main() {
     // rust cannot do this since it'll overflow
     // let a: i128 = 340282366920938463463374607431768211455 + 340282366920938463463374607431768211455;
 
-    let x = BigInt::new("340282366920938463463374607431768211456");
-    let y = BigInt::new("340282366920938463463374607431768211455");
-    let z = x + y;
+    let x = BigInt::new("2");
+    let y = BigInt::new("2");
+    let z = x - y;
     println!("{}", z);
 }
